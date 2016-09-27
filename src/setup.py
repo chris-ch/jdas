09fe11bc4216e3e030f2226423a0b3fa7f73ddf2
chris-ch <chris.perso@gmail.com>
2016-09-27 15:13:08 +0200

    'dev'

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='upwork-jdas',
+    version='1.0',
+    packages=[''],
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='Christophe',
+    author_email='',
+    description=''
+)
